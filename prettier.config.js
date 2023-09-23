ce334aed1eb6f3a014813d0465dd78c1180842a9
ziimir <ziimir9@gmail.com>
2023-09-23 15:26:18 +0300

    use prettier from package

--- prettier.config.js
+++ prettier.config.js
@@ -1,27 +1,3 @@
-module.exports = {
-    printWidth: 120,
-    tabWidth: 4,
-    useTabs: false,
-    semi: true,
-    singleQuote: true,
-    quoteProps: 'as-needed',
-    jsxSingleQuote: false,
-    trailingComma: 'all',
-    bracketSpacing: true,
-    bracketSameLine: false,
-    arrowParens: 'always',
-    overrides: [
-        {
-            files: ['*.json'],
-            options: {
-                tabWidth: 2,
-            },
-        },
-        {
-            files: ['*.html', '*.hbs'],
-            options: {
-                singleQuote: false,
-            },
-        },
-    ],
-};
+const prettierConfig = require('@ziimir/prettier-config');
+
+module.exports = prettierConfig;
